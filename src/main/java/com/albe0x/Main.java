a5155d88a2cc7f3ca627830737c011672b334d28
lai2-24 <lai2-24@itismeucci.com>
2025-10-01 10:25:31 +0200

    creato server

--- src/main/java/com/albe0x/Main.java
+++ src/main/java/com/albe0x/Main.java
@@ -1,7 +1,25 @@
 package com.albe0x;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         System.out.println("Hello world!");
+        ServerSocket ss = new ServerSocket(3000) ;
+        Socket s = ss.accept();
+        System.out.println("QUALCON OSI E' COLLEGATO");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        
+        String input = in.readLine();
+        out.println(input.toUpperCase());
+        
+        
     }
 }
