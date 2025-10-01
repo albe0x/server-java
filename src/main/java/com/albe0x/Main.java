1ad3ce8021d437326a6ce34be6940225fd93ff48
lai2-24 <lai2-24@itismeucci.com>
2025-10-01 10:42:57 +0200

    sd

--- src/main/java/com/albe0x/Main.java
+++ src/main/java/com/albe0x/Main.java
@@ -9,17 +9,21 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        System.out.println("Hello world!");
         ServerSocket ss = new ServerSocket(3000) ;
         Socket s = ss.accept();
         System.out.println("QUALCON OSI E' COLLEGATO");
 
         BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-        
-        String input = in.readLine();
-        out.println(input.toUpperCase());
-        
-        
+        while (true) { 
+            String input = in.readLine();
+            System.out.println(input);
+
+            if(input.compareTo("!") == 0){
+                return;
+            }
+
+            out.println(input.toUpperCase());
+        }
     }
 }
